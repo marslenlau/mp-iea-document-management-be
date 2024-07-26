4085372c1295cc96783719136e1ae5d28065e2d4
Marcelo Poma <marcelo.pc.dev@gmail.com>
2024-07-26 10:54:09 -0400

    add: iniciazliacion de configuracion de prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
     // Import the ConfigModule
     ConfigModule.forRoot({
       load: [EnvConfiguration]
     }),
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
